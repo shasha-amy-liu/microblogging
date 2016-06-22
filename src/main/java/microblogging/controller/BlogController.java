da133d240029a9d14346fa4735ee205164689f92
doudou <liushasha99@hotmail.com>
2016-06-21 20:45:46 -0400

    fix tracking

--- src/main/java/microblogging/controller/BlogController.java
+++ src/main/java/microblogging/controller/BlogController.java
@@ -1,11 +1,22 @@
 package microblogging.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import microblogging.model.Blog;
+import microblogging.model.User;
 import microblogging.service.BlogService;
 import microblogging.service.UserService;
+import microblogging.vo.BlogVO;
 
 @Controller
 @RequestMapping("blog")
@@ -31,21 +42,21 @@ public class BlogController {
 //
 //        return result;
 //    }
-//
-//    @RequestMapping(value = "/tracking", method = RequestMethod.GET)
-//    public @ResponseBody List<BlogVO> tracking(HttpServletRequest request, HttpServletResponse response) {
-//        // get user info
-//        String username = ((User) request.getSession().getAttribute("user")).getUsername();
-//
-//        List<Blog> blogs = userService.getBlogTracking(username);
-//        List<BlogVO> result = new ArrayList<BlogVO>();
-//        for (Blog blog : blogs) {
-//            result.add(new BlogVO(blog));
-//        }
-//
-//        return result;
-//    }
-//
+
+    @RequestMapping(value = "/tracking", method = RequestMethod.GET)
+    public @ResponseBody List<BlogVO> tracking(HttpServletRequest request, HttpServletResponse response) {
+        // get user info
+        String userId = ((User) request.getSession().getAttribute("user")).getId();
+
+        List<Blog> blogs = blogService.findByUserId(userId);
+        List<BlogVO> result = new ArrayList<BlogVO>();
+        for (Blog blog : blogs) {
+            result.add(new BlogVO(blog));
+        }
+
+        return result;
+    }
+
 //    @RequestMapping(value = "/add", method = RequestMethod.POST)
 //    public @ResponseBody BlogVO add(HttpServletRequest request, HttpServletResponse response,
 //            @RequestParam(value = "blogContent", required = true) String blogContent) {
